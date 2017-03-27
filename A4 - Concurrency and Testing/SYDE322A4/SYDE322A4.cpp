d74bbdb65a81006deacd3ca2bdc914afceb016f0
Glendon Ngo <g2ngo@uwaterloo.ca>
2017-03-27 02:07:08 -0400

    Changed to VS C++ project

---
+++ A4 - Concurrency and Testing/SYDE322A4/SYDE322A4.cpp
@@ -0,0 +1,16 @@
+// SYDE322A4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	std::cout << "Hello World" << std::endl;
+	std::cin >> x;
+
+    return 0;
+}
+
